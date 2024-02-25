56d9276819a6874eb1f4da9a914966204d6754c9
Robbie Muchiri <developedbyrobbie@gmail.com>
2024-02-25 15:06:07 +0300

    updated footer

--- client/src/layouts/footer.tsx
+++ client/src/layouts/footer.tsx
@@ -1,9 +1,10 @@
 import { ModeToggle } from "@/components/ui/mode-toggle"
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
       <div className="w-full flex justify-between border-t py-2 max-h-24">
-          <p>Footer</p>
+          <p>Built by <Link to={'#'} className="underline">Robbie</Link>. Source code available on <Link to={'https://github.com/whoisrobb/mismatchd-react'} className="underline">GitHub</Link></p>
           <ModeToggle />
       </div>
   )
