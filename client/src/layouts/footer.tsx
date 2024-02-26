9bdcd2b170a112965d78f2e0c54d336db40909e0
Robbie Muchiri <developedbyrobbie@gmail.com>
2024-02-26 10:47:53 +0300

    bug ixes

--- client/src/layouts/footer.tsx
+++ client/src/layouts/footer.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
       <div className="w-full flex justify-between border-t py-2 max-h-24">
-        <div className="lg:flex lg:items-center">
+        <div className="lg:flex lg:items-center gap-2">
           <p>Built by <Link to={'#'} className="underline">Robbie</Link>.</p>
           <p>Source code available on <Link to={'https://github.com/whoisrobb/mismatchd-react'} className="underline">GitHub</Link>.</p>
         </div>
